653c92e85077bfdba2358e24ea403994d5f43712
Arbona <jeanmichel.arbona@gmail.com>
2017-10-19 09:35:21 +0200

    Adding weight

--- src/test/evaluate.py
+++ src/test/evaluate.py
@@ -28,30 +28,45 @@ weights = "data/cluster/training/allign-agree-five-clean-B/my_model_weights-3990
 #weights = "data/training/test-single-base-bis/my_model_weights-0.h5"
 weights = "data/cluster/training/test-single-various-w-size-8bases-smallerw/my_model_weights-1800.h5"
 weights = "data/cluster/training/allign-agree-five-clean-B-smallB/my_model_weights-5990.h5"
-weights = "data/cluster/training/allign-agree-five-clean-B-smallB-test-ssample/my_model_weights-470.h5"
+weights = "data/cluster/training/allign-agree-five-clean-B-smallB-test-ssample/my_model_weights-2480.h5"
+weights = "data/cluster/training/test-single-various-w-size-8bases-smallw-fixed/my_model_weights-500.h5"
+
 
 basename = "results/v9p5-best-B-20170908-R9.5-newchem-test-clean-window_size/"
-basename = "results/v9p5-best-B-20170908-R9.5-froms-two-200-seg-last-smaller-explo-sm/"
 
+weights = "data/cluster/training/allign-agree-85555/my_model_weights-3900.h5"
+basename = "results/v9p5-best-B-20170908-R9.5-froms-two-200-seg-last-smaller-explo-bw/"
+
+"""
+weights = "data/cluster/training/allign-agree-63333/my_model_weights-1160.h5"
+basename = "results/v9p5-best-B-20170908-R9.5-froms-two-200-seg-last-smaller-explo-sw/"
+
+
+weights = "data/cluster/training/allign-agree-85555-8b/my_model_weights-1480.h5"
+basename = "results/v9p5-best-B-20170908-R9.5-froms-two-200-seg-last-smaller-explo-8test/"
+
+weights = "data/cluster/training/allign-agree-63333-8b/my_model_weights-1400.h5"
+basename = "results/v9p5-best-B-20170908-R9.5-froms-two-200-seg-last-smaller-explo-8test/"
+"""
 ref = "data/external/ref/S288C_reference_sequence_R64-2-1_20150113.fa"
 redo = 1
 # Evaluate all the sample
 list_dir = [["substituted", "sub_template", 5], ["control", "control_template", 5],
             ["control-k47211", "control-k47211_template", 5]]
 
-list_dir = [["20170908-R9.5/AD-basecalled", "20170908-R9.5/prout", 3],
-            ["20170908-R9.5/AG-basecalled", "20170908-R9.5/BTF_AG_ONT_1_FAH14273_A-select_pass", 6],
-            ["20170908-R9.5/AH-basecalled", "20170908-R9.5/BTF_AH_ONT_1_FAH14319_A-select_pass", 3],
+list_dir = [["20170908-R9.5/AD-basecalled", "20170908-R9.5/prout", 5],
+            ["20170908-R9.5/AG-basecalled", "20170908-R9.5/BTF_AG_ONT_1_FAH14273_A-select_pass", 8],
+            ["20170908-R9.5/AH-basecalled", "20170908-R9.5/BTF_AH_ONT_1_FAH14319_A-select_pass", 5],
             ["20170908-R9.5/AG-Thy/", "20170908-R9.5/BTF_AG_ONT_1_FAH14273_A-select_pass", 5],
             ["20170908-R9.5/AH-BrdU/", "20170908-R9.5/BTF_AH_ONT_1_FAH14319_A-select_pass", 5],
-            ["20170908-R9.5/AI-CldU/0/", "20170908-R9.5/BTF_AI_ONT_1_FAH14242_A-select_pass", 4],
-            ["20170908-R9.5/AK-EdU/0/", "20170908-R9.5/BTF_AK_ONT_1_FAH14211_A-select_pass", 4],
-            ["20170908-R9.5/AL-IdU/0/", "20170908-R9.5/BTF_AL_ONT_1_FAH14352_A-select_pass", 4]]
-for dire, out, w in list_dir[:3]:  # + list_dir[4:]:
+            ["20170908-R9.5/AI-CldU/0/", "20170908-R9.5/BTF_AI_ONT_1_FAH14242_A-select_pass", 5],
+            ["20170908-R9.5/AK-EdU/0/", "20170908-R9.5/BTF_AK_ONT_1_FAH14211_A-select_pass", 5],
+            ["20170908-R9.5/AL-IdU/0/", "20170908-R9.5/BTF_AL_ONT_1_FAH14352_A-select_pass", 5]]
+for dire, out, w in list_dir[:3]:  # + list_dir[5:]:  # + list_dir[5:]:
     if redo:
         process(weights, directory="data/raw/%s/" % dire,
                 output="data/processed/{0}{1}.fasta".format(basename, out), Nbases=5, reads="",
-                filter=None, already_detected=False, Nmax=20, size=20,
+                filter=None, already_detected=False, Nmax=200, size=20,
                 n_output_network=1, n_input=1, chemistry="rf", window_size=w)
         # filter="data/processed/%s.InDeepNano.test" % outz , already_detected=False)
 
