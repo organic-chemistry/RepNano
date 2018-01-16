bd71892f0c76b4618e1ea30e0ecd10eeaaa54314
Arbona <jeanmichel.arbona@gmail.com>
2018-01-16 11:35:29 +0100

    Deeper residual network

--- src/models/model_reverse.py
+++ src/models/model_reverse.py
@@ -65,7 +65,6 @@ def build_models(size=4, nbase=1, trainable=True, ctc_length=40, ctc=True,
                                 trainable=trainable, recurrent_dropout=recurrent_dropout),
                            merge_mode=merge_mode)(inputs)
         # if res:
-        l1 = Add()([l1, inputs])
         l2 = Bidirectional(LSTM(size, return_sequences=True,
                                 trainable=trainable, recurrent_dropout=recurrent_dropout),
                            merge_mode=merge_mode)(l1)
