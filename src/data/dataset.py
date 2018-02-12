bac77a434ef28be815aeaa74ffb2efc8ae6e17e7
Arbona <jeanmichel.arbona@gmail.com>
2018-02-12 15:12:29 +0100

    fixing

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -152,7 +152,7 @@ class Strand:
 
         if f == "no_basecall":
             sig = self.segmentation(w=window_size)
-            names = ["seq", "mean", "stdv", "length", "start"]
+            names = ["mean", "stdv", "length", "start"]
 
             return pd.DataFrame({n: sig[n] for n in names}).convert_objects(convert_numeric=True)
 
