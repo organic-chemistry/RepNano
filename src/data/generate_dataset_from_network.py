a3becef5f4b03f220f81432e66503bd857c6e6ef
Arbona <jeanmichel.arbona@gmail.com>
2018-02-14 14:22:55 +0100

    generate-from-ntwk

--- src/data/generate_dataset_from_network.py
+++ src/data/generate_dataset_from_network.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--window-size', dest="window_size", type=int, choices=[4, 5, 8], default=5)
+    parser.add_argument('--Nbases', type=int, choices=[4, 5, 8], default=4)
+
     parser.add_argument('--root', type=str, default="data/training/")
     parser.add_argument('--test', dest='test', action='store_true')
     parser.add_argument('--size', type=int, default=20)
