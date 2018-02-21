1d84da6e18064e63c84504864108fe769bda55ab
Arbona <jeanmichel.arbona@gmail.com>
2018-02-21 14:38:04 +0100

    adding tv evaluate

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -238,7 +238,8 @@ if __name__ == '__main__':
     parser.add_argument('--maxlen_input', dest="maxlen_input", type=int, default=None)
     parser.add_argument('--target', dest='target', type=str, default=None)
     parser.add_argument('--no-allign', dest='no_allign', action="store_true")
-    parser.add_argument("--method", dest="method", choices=["FW", "TV", "TV45"], default="FW")
+    parser.add_argument("--method", dest="method",
+                        choices=["FW", "TV", "TV45", "TV25"], default="FW")
 
     args = parser.parse_args()
 
