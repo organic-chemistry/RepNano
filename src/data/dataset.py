306abfc5b8518a0bef69457a1a08d246f21a08a5
Arbona <jeanmichel.arbona@gmail.com>
2018-02-22 09:59:28 +0100

    adding tv evaluate

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -498,6 +498,7 @@ class Strand:
                         stl_base[where] += b
                         stl_base[where] = stl_base[where].replace("N", "")
                         # First try to transfer at previouss:
+                        print(stl_base[where - 1], stl_base[where], stl_base[where + 1])
                         if len(stl_base[where]) > 2 and where > 0:
                             if "NN" in stl_base[where - 1]:
 
@@ -517,6 +518,8 @@ class Strand:
                                 "N" * (2 - len(stl_base[where][2:]))
 
                         stl_base[where] = stl_base[where][:2]
+                        print(stl_base[where - 1], stl_base[where], stl_base[where + 1])
+
                         #print("After", stl_base[where])
                     else:
                         stl_base[where] = b
