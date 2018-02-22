7ed0bcfcb11a856d760bd22699be1bf246288f2b
Arbona <jeanmichel.arbona@gmail.com>
2018-02-22 09:56:03 +0100

    adding tv evaluate

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -507,7 +507,7 @@ class Strand:
                             elif "N" in stl_base[where - 1]:
                                 stl_base[where - 1] += stl_base[where][:1]
                                 stl_base[where - 1] = stl_base[where - 1].replace("N", "")
-
+                                print(stl_base[where - 1])
                                 stl_base[where] = stl_base[where][1:]
 
                         if len(stl_base[where]) > 2 and where < len(stl_base):
