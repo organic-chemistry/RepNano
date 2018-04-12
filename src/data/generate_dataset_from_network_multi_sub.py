7fc0cd6f6da37673068549f22e168bc57a0e5c07
Arbona <jeanmichel.arbona@gmail.com>
2018-04-12 16:19:32 +0200

    generate_from_ntwk

---
+++ src/data/generate_dataset_from_network_multi_sub.py
@@ -0,0 +1,28 @@
+if __name__ == "__main__":
+    import argparse
+
+    import os
+    import numpy as np
+    # rom subprocess import PIPE
+    # OMP_NUM_THREADS=1 THEANO_FLAGS=mode=FAST_RUN
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--command-line', dest="command_line", type=str)
+    parser.add_argument("--n-cpu", dest="n_cpu", type=int, default=None)
+    parser.add_argument("--range", dest="range", nargs='+', default=[], type=float)
+    parser.add_argument("--name", dest="name", type=str, default='dataset.pick')
+    parser.add_argument("--target", dest="target", type=str, default='dataset.pick')
+
+    args = parser.parse_args()
+
+    os.makedirs(os.path.split(args.name)[1], exist_ok=True)
+
+    for i in range(args.n_cpu):
+        start = np.arange(args.range[0], args.range[1] + 1e-4,
+                          (args.range[1] - args.range[0]) / args.n_cpu)[i]
+        end = np.arange(args.range[0], args.range[1] + 1e-4,
+                        (args.range[1] - args.range[0]) / args.n_cpu)[i + 1]
+
+        cmd = "qsub -v TARGET=%s,START=%.3f,END=%.3f,NAME=%s script_generate.sh" % (
+            args.target, start, end, args.name)
+        print(cmd)
+        os.popen(cmd)
