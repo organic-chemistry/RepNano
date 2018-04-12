6d22e3123112a168f9ca20c96360ee48d3bb3ab4
Arbona <jeanmichel.arbona@gmail.com>
2018-04-12 16:30:57 +0200

    generate_from_ntwk

--- src/data/generate_dataset_from_network_multi_sub.py
+++ src/data/generate_dataset_from_network_multi_sub.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     parser.add_argument("--range", dest="range", nargs='+', default=[0, 1], type=float)
     parser.add_argument("--name", dest="name", type=str, default='dataset.pick')
     parser.add_argument("--target", dest="target", type=str, default='dataset.pick')
-    parser.add_argument("--weight", dest="weights", type=str, default='')
+    parser.add_argument("--weight", dest="weight", type=str, default='')
     parser.add_argument("--test", dest="test", action="store_true")
 
     args = parser.parse_args()
