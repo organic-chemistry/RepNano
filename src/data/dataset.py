8c2133c79305b7d514959de8508c5288cb7f4324
Arbona <jeanmichel.arbona@gmail.com>
2018-04-30 15:59:06 +0200

    param sscore

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -498,7 +498,7 @@ class Strand:
             alph = "ACGTN"
             print(len(pre),)
             print(len(pre[0]))
-            om1, om2 = pre
+            om1, om2, _ = pre
             om1 = np.argmax(om1[0], axis=-1)
             om2 = np.argmax(om2[0], axis=-1)
 
