52d3d18c296cf456da3e10ce57abfa6f79bc53b2
Arbona <jeanmichel.arbona@gmail.com>
2018-04-30 14:48:28 +0200

    param sscore

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -577,7 +577,7 @@ class Strand:
                         stl_base[where] = stl_base[where].replace("N", "")
                         # First try to transfer at previouss:
                         #print(stl_base[where - 1], stl_base[where], stl_base[where + 1])
-                        if len(stl_base[where]) >= 2 and where > 0:
+                        if len(stl_base[where]) > 2 and where > 0:
                             if "NN" in stl_base[where - 1]:
 
                                 #
@@ -588,6 +588,13 @@ class Strand:
                                 stl_base[where - 1] = stl_base[where - 1].replace("N", "")
                                 #print(stl_base[where - 1])
                                 stl_base[where] = stl_base[where][1:]
+
+                        elif len(stl_base[where]) == 2 and where > 0:
+                            if "NN" in stl_base[where - 1]:
+
+                                #
+                                stl_base[where - 1] = stl_base[where][:2]
+                                stl_base[where] = stl_base[where][2:]
                                 # print(stl_base[where])
 
                         if len(stl_base[where]) > 2 and where < len(stl_base):
