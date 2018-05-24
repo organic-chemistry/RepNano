1886a0aec897eb512948941d962230639caf1eb6
Arbona <jeanmichel.arbona@gmail.com>
2018-05-24 09:09:44 +0200

    flatten

--- src/models/model.py
+++ src/models/model.py
@@ -230,7 +230,7 @@ def build_models(size=20, nbase=1, trainable=True, ctc_length=40, ctc=True,
                     #l3b = Concatenate()([out_layer1, out_layer2])
                     #out_layer1 = Reshape((input_length * 2, Nbases))(l3b)
 
-                    extd = Concatenate(axis=-1)([ext[n], out_layer1[::2, ::]])
+                    extd = Concatenate(axis=-1)([ext[n], out_layer1[::, ::2, ::]])
                     extd1 = Reshape((input_length * 2, 1))(extd)
                     ot.append(Lambda(average, output_shape=average_output_shape,
                                      name="o%i" % n)([extd1, out_layer1]))
