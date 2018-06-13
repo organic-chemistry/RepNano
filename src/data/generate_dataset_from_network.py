916d87f53766d2814d82475c23fcb3b31928c792
Arbona <jeanmichel.arbona@gmail.com>
2018-06-13 10:03:08 +0200

    human

--- src/data/generate_dataset_from_network.py
+++ src/data/generate_dataset_from_network.py
@@ -297,9 +297,9 @@ if __name__ == "__main__":
         strand.changed = True
         score = strand.score("".join(transfered["seq_ref"]).replace(
             "N", "").replace(sub, "T"), ref, all_info=False)
-        print("ALL", score)
-        print("".join(transfered["seq_ref"]).replace(
-            "N", "").replace(sub, "T"))
+        print("ALL", al)
+        # print("".join(transfered["seq_ref"]).replace(
+        #    "N", "").replace(sub, "T"))
         return transfered, al[2] / len(bc_strand), score, len(ref)
         # except:
 
