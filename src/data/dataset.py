197fdd661e76d38ff150da2fc0633e0ae0ee8446
Arbona <jeanmichel.arbona@gmail.com>
2018-10-22 16:18:14 +0200

    adding overlap

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -582,6 +582,7 @@ class Strand:
                         print("vaules", np.sum(om1 == 4), np.sum(om1 == 4) / np.sum(toub))
                     om1 = np.argmax(pre[0], axis=-1)
                     toub = (om1 == 3) | (om1 == 4)
+                    ptb = ptb[:len(toub)]
                     om1[toub & (ptb > 0.5)] = 4
                     om1[toub & (ptb < 0.5)] = 3
                     signal = signal[:len(om1)]
