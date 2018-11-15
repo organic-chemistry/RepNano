f1eaaaa9823dbe92d14b6c892107a984b7d2ff9c
Arbona <jeanmichel.arbona@gmail.com>
2018-11-15 12:59:14 +0100

    simpler_model

--- src/models/simple.py
+++ src/models/simple.py
@@ -184,7 +184,7 @@ for val in indep_val:
 
 print(train_test)
 print(indep_val)
-_, X_train, _, y_train = load_data_complete(train_test, root=root, per_dataset=100, lenv=200)
+_, X_train, _, y_train = load_data_complete(train_test, root=root, per_dataset=200, lenv=200)
 _, X_val, _, y_val = load_data_complete(indep_val, root=root, per_dataset=40, lenv=200)
 
 print(X_train.shape, y_train.shape)
