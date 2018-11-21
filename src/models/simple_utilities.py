14519d0532104129dd5fdf9851c32f9eba7d6477
jarbona <jeanmichel.arbona@ens-lyon.fr>
2018-11-21 11:44:46 +0100

    predict with ratio

---
+++ src/models/simple_utilities.py
@@ -0,0 +1,31 @@
+def load_data(lfiles, values=["saved_weights_ratio.05-0.03.hdf5","init_B"], root=".", per_dataset=None):
+    X = []
+    y = []
+    for file in lfiles:
+        d = pd.read_csv(file)
+        #print(file, d)
+        X1 = [root + "/" + f for f in d["filename"]]
+        found = False
+        for value in values:
+            if value in d.columns:
+
+                y1 = d[value]
+                print("using value",value)
+                break
+        if not found:
+            print("Values not found",values)
+            raise
+        #print(np.mean(y1), np.std(y1),len(y1),len(X1))
+        yw = d["init_w"]
+        #print("Weight", np.mean(yw),len(yw))
+        y1 = [[iy1, iyw] for iy1, iyw in zip(y1, yw)]
+        #print(len(y1))
+        if per_dataset is None:
+            X.extend(X1)
+            y.extend(y1)
+        else:
+            X.extend(X1[:per_dataset])
+            y.extend(y1[:per_dataset])
+    assert (len(X) == len(y))
+    #print(y)
+    return X, y
