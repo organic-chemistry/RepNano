c57651bdc23060c5a13d1f7a9682b763c5a7a3c0
jarbona <jeanmichel.arbona@ens-lyon.fr>
2018-11-22 14:31:51 +0100

    pmix

---
+++ src/models/filter_BT.py
@@ -0,0 +1,75 @@
+from simple_utilities import load_data,load_events,transform_reads
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+
+
+def model(typem=1):
+    if typem==1:
+
+        model = Sequential()
+        # model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+        model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(200, 1)))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(LSTM(100))
+        model.add(Dense(1, activation='linear'))
+        model.compile(loss='mse', optimizer='adam')  # , metrics=['accuracy'])
+        ntwk=model
+    return ntwk
+
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--file', dest='filename', type=str)
+parser.add_argument('--weight-name', dest='weight_name', type=str)
+parser.add_argument('--typem', dest='typem', type=int,default=1)
+parser.add_argument('--maxf', dest='max', type=int,default=None)
+parser.add_argument('--window-length', dest='length_window', type=int,default=200)
+
+
+args = parser.parse_args()
+
+filename=args.filename
+length_window=200
+maxf=args.maxf
+weight_name=args.weight_name
+typem=args.typem
+
+X,y = load_data([filename],root=root)
+y = np.array(y)[::,0]
+Xr,yr,fn = load_events(X[:maxf],y[:maxf],min_length=length_window)
+Xt,yt =transform_reads(Xr,yr,lenv=length_window)
+
+
+ntwk = model(typem=typem)
+ntwk.load_weights(weight_name)
+
+Predicts = []
+for xt in Xt:
+    Predicts.append(np.mean(ntwk.predict(xt[np.newaxis, ::, ::])))
+
+
+Predicts = np.array(Predicts)
+pylab.hist(Predicts,bins=40)
+print("Percent of reads < 0.3",np.sum(Predicts<0.3)/np.sum(Predicts))
+pylab.savefig("histo_B_T_%s"%(weight_name[:-5])+args.extra)
+
+csvf = pd.read_csv(filenanme)
+wn = aweight_name[:-5]
+csvf[wn] = [1 for _ in range(len(csvf))]
+
+assert len(Predicts) == len(fn)
+for f,r in zip(fn,Predicts):
+    if r > 0.3:
+        b=1
+    else:
+        b=0
+    fnshort = f.replace("/data/bioinfo@borvo/users/jarbona/deepnano5bases/data/raw/","")
+    csvf.loc[csvf["filename"]==fnshort,wn]=b
+
+csvf.to_csv(filename, index=False)
