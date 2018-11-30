41d16190d568dfad6501efc41e75bdc37b1224d6
jarbona <jeanmichel.arbona@ens-lyon.fr>
2018-11-30 09:52:25 +0100

    pmix

--- src/models/simple.py
+++ src/models/simple.py
@@ -44,6 +44,7 @@ def load_data_complete(dataset, root, per_dataset=None, lenv=200,
         Xpp, ypp = transform_reads(Xp, np.array(yp), lenv=lenv,delta=delta,rescale=rescale)
         Xpp = np.concatenate(Xpp, axis=0)
         ypp = np.concatenate(ypp, axis=0)
+        print("Total cumulated read length_after_cut",np.sum([xi.shape[0] for xi in Xpp]))
 
         X_t.append(Xpp)
         y_t.append(ypp)
