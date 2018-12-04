ba04d4da1c6bfa618561bfe4ce2a93a8a443f74c
jarbona <jeanmichel.arbona@ens-lyon.fr>
2018-12-04 13:30:12 +0100

    pmix

--- src/models/simple_utilities.py
+++ src/models/simple_utilities.py
@@ -189,7 +189,7 @@ def transform_reads(X, y, lenv=200,max_len=None,overlap=None,delta=False,rescale
             yip[::] = yi
 
         if noise:
-            V[::,::,0] *= 0.9+0.2*(1-2*np.random.rand(len(V)))[::,np.newaxis]
+            V[::,::,0] *= (0.9+0.2*np.random.rand(len(V))[::,np.newaxis])
 
         Xt.append(V)
         yt.append(yip)
